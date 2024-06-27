c8aa6063b6581c06a6d9dbd8be46c80feff60f3f
fnxsdummy <147632085+fnxsdummy@users.noreply.github.com>
2024-06-27 15:16:24 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function(){
+    $('.single-item').slick({
+        dots: true,
+        autoplay: true,
+
+    });
+  });
+  
+  
+  function scrl() {
+    var qx = document.getElementById("scroll-about");
+  qx.scrollIntoView();
+  };
